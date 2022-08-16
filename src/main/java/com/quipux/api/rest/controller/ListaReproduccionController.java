6879eedcb884e4a5547c41d51a8a2beb570af50d
Andres Meneses <andmen1920@gmail.com>
2022-08-16 12:47:13 -0500

    permitiendo conexion de proyecto angular

--- src/main/java/com/quipux/api/rest/controller/ListaReproduccionController.java
+++ src/main/java/com/quipux/api/rest/controller/ListaReproduccionController.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.quipux.api.rest.model.Lista;
 import com.quipux.api.rest.services.ListaService;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
 @RequestMapping("/api/listaReproduccion")
 public class ListaReproduccionController {
 	
